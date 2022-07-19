8c2949d18fa98903f97d27567aedd7f3d68e3914
John <john.mbiddulph@gmail.com>
2022-07-19 23:20:38 +0100

    added vuelidate and styled form fields to highlight fields that need attention

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -14,7 +14,7 @@
             <div class="col-12">
               <h5>Title</h5>
               <label for="Title">
-                <select class="form-select" aria-label="Default select example" v-model="post.title" name="title" required>
+                <select class="form-select" aria-label="Default select example" v-model="post.title" name="title">
                   <option selected>Select...</option>
                   <option value="Mr">Mr</option>
                   <option value="Ms">Ms</option>
@@ -28,23 +28,38 @@
             <div class="col-12 form-group">
               <h5>First Name</h5>
               <label for="Name" class="w-full">
-                <input class="form-control input-lg" type="text" placeholder="Jane" v-model="post.first_name" name="first_name" required>
+                <div :class="{ error: v$.post.first_name.$errors.length }">
+                  <input class="form-control input-lg" type="text" placeholder="Jane" v-model="post.first_name">
+                  <div class="input-errors" v-for="error of v$.post.first_name.$errors" :key="error.$uid">
+                    <small class="text-red-600 error-msg">Please enter a first name</small>
+                  </div>
+                </div>
               </label>
             </div>
           </div>
           <div class="row py-3">
             <div class="col-12 form-group">
               <h5>Last Name</h5>
-              <label for="Name" class="w-full">
-                <input class="form-control input-lg" type="text" placeholder="Smith" v-model="post.last_name" name="last_name" required>
-              </label>
+                <label for="Name" class="w-full">
+                  <div :class="{ error: v$.post.last_name.$errors.length }">
+                    <input class="form-control input-lg" type="text" placeholder="Smith" v-model="post.last_name" name="last_name">
+                    <div class="input-errors" v-for="error of v$.post.last_name.$errors" :key="error.$uid">
+                      <small class="text-red-600 error-msg">Please enter a last name</small>
+                    </div>
+                  </div>
+                </label>
             </div>
           </div>
           <div class="row py-3">
             <div class="col-12">
               <h5>Premises Address</h5>
               <label for="address" class="w-full">
-                <textarea cols="20" rows="4" class="form-control input-lg" type="text" placeholder="Search or tell use what kind of business this is" v-model="post.address" name="address" required></textarea>
+                <div :class="{ error: v$.post.address.$errors.length }">
+                  <textarea cols="20" rows="4" class="form-control input-lg" type="text" placeholder="Search or tell use what kind of business this is" v-model="post.address" name="address"></textarea>
+                  <div class="input-errors" v-for="error of v$.post.address.$errors" :key="error.$uid">
+                    <small class="text-red-600 error-msg">Please enter valid address</small>
+                  </div>
+                </div>
               </label>
             </div>
           </div>
@@ -85,7 +100,12 @@
             <div class="col-12">
               <h5>Password</h5>
               <label for="password">
-                <input class="form-control" type="password" placeholder="password" v-model="post.password" name="password" required>
+                <div :class="{ error: v$.post.password.$errors.length }">
+                  <input class="form-control" type="password" placeholder="password" v-model="post.password" name="password">
+                  <div class="input-errors" v-for="error of v$.post.password.$errors" :key="error.$uid">
+                    <small class="text-red-600 error-msg">Please enter password</small>
+                  </div>
+                </div>
               </label>
             </div>
           </div>
@@ -93,7 +113,12 @@
             <div class="col-12">
               <h5>Confirm Password</h5>
               <label for="password_confirmation">
-                <input class="form-control" type="password" placeholder="Confirm Password" v-model="post.password_confirmation" name="password_confirmation" required>
+                <div :class="{ error: v$.post.password_confirmation.$errors.length }">
+                  <input class="form-control" type="password" placeholder="Confirm Password" v-model="post.password_confirmation" name="password_confirmation">
+                  <div class="input-errors" v-for="error of v$.post.password_confirmation.$errors" :key="error.$uid">
+                    <small class="text-red-600 error-msg">The password does not match</small>
+                  </div>
+                </div>
               </label>
             </div>
           </div>
@@ -107,7 +132,8 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import FormBottom from '@/components/FormBottom.vue';
-
+import useVuelidate from '@vuelidate/core';
+import {required, sameAs} from '@vuelidate/validators';
 
 export default defineComponent({
   name: 'RegisterForm',
@@ -116,6 +142,7 @@ export default defineComponent({
   },
   data() {
     return {
+      v$: useVuelidate(),
       post: {
         title: null,
         first_name: null,
@@ -129,16 +156,32 @@ export default defineComponent({
       }
     }
   },
+  validations () {
+    return {
+      post: {
+        first_name: {required},
+        last_name: {required},
+        address: {required},
+        password: {required},
+        password_confirmation: sameAs(this.post.password)
+      }
+    }
+  },
   methods: {
     submit() {
-      console.warn(this.post)
-      this.axios.post('https://dr-frontend-test-api.herokuapp.com/api/accounts', this.post)
-      .then((result) => {
-        console.log('result: ', result);
-        if(result.status === 200) {
-           this.$router.push({ path : '/thankyou' });
-        }
-      })
+      console.log('V: ',this.v$)
+      this.v$.$validate()
+      if (!this.v$.$error) {
+        this.axios.post('https://dr-frontend-test-api.herokuapp.com/api/accounts', this.post)
+        .then((result) => {
+          console.log('result: ', result);
+          if(result.status === 200) {
+            this.$router.push({ path : '/thankyou' });
+          }
+        })
+      } else {
+        alert('Form failed validation')
+      }
     }
   },
   computed: {
@@ -150,6 +193,9 @@ export default defineComponent({
 </script>
 
 <style scoped lang="scss">
+.error input, .error textarea {
+  border: 1px solid #ff0000;
+}
 .top-shadow {
   box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
 }
